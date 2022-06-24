d182316f98507b3011fc1dd7390733a990ad9e64
Eri Schon <erischon@gmail.com>
2022-06-24 10:52:10 +0200

    sass

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header>
-      <div className="container">
-        <Link to="/">
-          <h1>My Bookcase</h1>
-        </Link>
-      </div>
+      <Link to="/" className="logo">
+        My Bookcase
+      </Link>
     </header>
   );
 };
